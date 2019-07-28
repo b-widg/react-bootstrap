6141c740e966a61ca8a34a9cebccedb683cfc48f
b-widg <b-widg@users.noreply.github.com>
2019-07-27 18:29:04 -0700

    saving updates

---
+++ src/Home.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h2>Hello Pirate World</h2>
+      <p>
+        Pirate spirits jib prow square-rigged Sail ho Letter of Marque belay
+        Blimey rope's end six pounders chantey reef sails sutler gally Gold Road
+        measured fer yer chains nipperkin knave long boat quarter clap of
+        thunder chandler furl lee spanker flogging bring a spring upon her cabl
+      </p>
+      <p>
+        Pirate spirits jib prow square-rigged Sail ho Letter of Marque belay
+        Blimey rope's end six pounders chantey reef sails sutler gally Gold Road
+        bilge water skysail fire ship topgallant Plate Fleet ballast hogshead
+        scallywag Pieces of Eight execution dock flogging swab transom lugsail
+        gunwalls Brethren of the Coast fluke Cat o'nine tails Chain Shot cackle
+        fruit Letter of Marque coxswain long clothes salmagundi shrouds Spanish
+        Main heave down sheet driver yo-ho-ho gabion gaff snow Jack Ketch
+        measured fer yer chains nipperkin knave long boat quarter clap of
+        thunder chandler furl lee spanker flogging bring a spring upon her cabl
+      </p>
+      <p>
+        Pirate spirits jib prow square-rigged Sail ho Letter of Marque belay
+        Blimey rope's end six pounders chantey reef sails sutler gally Gold Road
+        bilge water skysail fire ship topgallant Plate Fleet ballast hogshead
+        scallywag Pieces of Eight execution dock flogging swab transom lugsail
+        gunwalls Brethren of the Coast fluke Cat o'nine tails Chain Shot cackle
+        fruit Letter of Marque coxswain long clothes salmagundi shrouds Spanish
+        Main heave down sheet driver yo-ho-ho gabion gaff snow Jack Ketch
+        measured fer yer chains nipperkin knave long boat quarter clap of
+        thunder chandler furl lee spanker flogging bring a spring upon her cabl
+      </p>
+    </div>
+  );
+};
